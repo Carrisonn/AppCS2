6a70cba12db0737b79b41e4556332826a8a619c6
Carlos <carlos97181034535@gmail.com>
2025-01-24 20:02:31 +0100

    API results are displayed on screen

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /** Global **/
 const searchInput = document.querySelector('#search-input');
+const searchButton = document.querySelector('#search-button');
 const resultsDiv = document.querySelector('#results');
 const form = document.querySelector('#form');
 
@@ -27,7 +28,6 @@ function validateSkin(event) {
     } 
 
     skinObj.skin = event.target.value.trim().toLowerCase();
-    console.log(skinObj);
 }
 
 async function getSkins(event) {
@@ -46,37 +46,79 @@ async function getSkins(event) {
         const data = await response.json();
         showData(data);
     } catch (error) {
-        console.log(error);
-    }
+        console.log(error) ?? showAlert('Ocurrió un error');
 
+        setTimeout(() => {
+            window.location.reload();
+        }, 3000);
+    }
 }
 
 function showData(data) {
-
     cleanResults();
-
+    
     const resultsTitle = document.createElement('h2');
     resultsTitle.classList.add('results-title', 'no-margin');
     resultsTitle.textContent = 'Resultados';
     resultsDiv.appendChild(resultsTitle);
 
-    data.forEach(info => {
+    let foundResults = false;
+    
+    data.forEach( info => {
         info.name = info.name.replace('| ', '').replace('★ ', '');
 
         if(info.weapon.name.toLowerCase().includes(skinObj.skin) || info.name.toLowerCase().includes(skinObj.skin) ) {
+            foundResults = true;
             resultsDiv.classList.remove('display-none');
 
-
-
-            const p = document.createElement('p');
-            p.textContent = info.name;
+            const divSkin = document.createElement('div');
+            divSkin.classList.add('div-skin');
             
+            const nameSkin = document.createElement('p');
+            nameSkin.classList.add('info-api','no-margin', 'margin-bottom');
+            nameSkin.innerHTML = `Nombre: <span class="info-skin">${info.name}</span>`;
             
-            resultsDiv.appendChild(p);
-            return;
+            const categoryName = document.createElement('p');
+            categoryName.classList.add('info-api','no-margin', 'margin-bottom');
+            categoryName.innerHTML = `Categoría: <span class="info-skin">${info.category.name}</span>`;
+            
+            const rarityName = document.createElement('p');
+            rarityName.classList.add('info-api','no-margin', 'margin-bottom');
+            rarityName.innerHTML = `Rareza: <span class="info-skin">${info.rarity.name}</span>`;
+
+            const stattrak = document.createElement('p');
+            stattrak.classList.add('info-api','no-margin', 'margin-bottom');
+            stattrak.innerHTML = `StatTrak: <span class="info-skin">${info.stattrak === true ? 'Sí' : 'No'}</span>`;
+
+            const souvenir = document.createElement('p');
+            souvenir.classList.add('info-api','no-margin', 'margin-bottom');
+            souvenir.innerHTML = `Souvenir: <span class="info-skin">${info.souvenir === true ? 'Sí' : 'No'}</span>`;
+
+            const exclusivity = document.createElement('p');
+            exclusivity.classList.add('info-api','no-margin', 'margin-bottom');
+            exclusivity.innerHTML = `Exclusividad: <span class="info-skin">${info.team.name}</span>`;
+
+            const imageURL = document.createElement('p');
+            imageURL.classList.add('info-api','no-margin', 'margin-bottom');
+            imageURL.innerHTML = `Imagen: <a href="${info.image}" class="image-skin" target="_blank">Click aquí</a>`;
+
+            divSkin.appendChild(nameSkin);
+            divSkin.appendChild(categoryName);
+            divSkin.appendChild(rarityName);
+            divSkin.appendChild(stattrak);
+            divSkin.appendChild(souvenir);
+            divSkin.appendChild(exclusivity);
+            divSkin.appendChild(imageURL);
+
+            resultsDiv.appendChild(divSkin);
         }
-        
     });
+
+    if(!foundResults) {
+        showAlert('No se encontraron resultados');
+        resultsDiv.classList.add('display-none');
+    }
+
     searchInput.value = '';
     skinObj.skin = '';
 }
@@ -89,10 +131,10 @@ function showAlert(message) {
     error.textContent = message;
     form.appendChild(error);
 
-    closeAlert();
+    closeError();
 }
 
-function closeAlert() {
+function closeError() {
     const closeError = document.querySelector('.error');
 
     setTimeout(() => {
