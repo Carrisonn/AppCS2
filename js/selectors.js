68665d1c89fa53745a06b0a383bb7e68babd49a7
Carlos <carlos97181034535@gmail.com>
2025-01-28 23:32:03 +0100

    first commit

---
+++ js/selectors.js
@@ -0,0 +1,12 @@
+export const searchInput = document.querySelector('#search-input');
+export const resultsDiv = document.querySelector('#results');
+export const form = document.querySelector('#form');
+export const menuBurgerIcon = document.querySelector('#menu-burger-icon');
+export const sideBar = document.querySelector('#sidebar');
+export const closeSideBarBtn = document.querySelector('#close-sidebar-btn');
+export const divNavLinks = document.querySelector('#div-nav-links');
+
+
+export const skinObj = {
+    skin: ''
+}
