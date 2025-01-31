a56bbab4010c4bc16f0df45fd58272e2d7a8972f
Carlos <carlos97181034535@gmail.com>
2025-01-31 02:16:02 +0100

    renamed file and added an object to the crate types

---
+++ js/constants.js
@@ -0,0 +1,27 @@
+export const searchInput = document.querySelector('#search-input');
+export const resultsDiv = document.querySelector('#results');
+export const form = document.querySelector('#form');
+export const menuBurgerIcon = document.querySelector('#menu-burger-icon');
+export const sideBar = document.querySelector('#sidebar');
+export const closeSideBarBtn = document.querySelector('#close-sidebar-btn');
+export const divNavLinks = document.querySelector('#div-nav-links');
+
+
+export const skinObj = {
+    skin: ''
+}
+
+export const stickerObj = {
+    sticker: ''
+}
+
+export const crateObj = {
+    crate: ''
+}
+export const crateTypeObj = {
+    'case': 'caja',
+    'sticker capsule': 'cápsula de stickers',
+    'souvenir': 'souvenir',
+    'autograph capsule': 'cápsula de autógrafos',
+    'patch capsule': 'cápsula de parches',
+}
